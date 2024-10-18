2b27b4824c892fbe425e92051014fb8db3909e4b
Jakiya-18 <jakiyabanu35@gmail.com>
2024-10-18 12:06:52 +0530

    variables file

---
+++ Variable.java
@@ -0,0 +1,77 @@
+public class Variable{
+    public static void main(String[] args){
+        int rollNo = 22;
+        System.out.println(rollNo); // declaration of variable and initializing a value
+        rollNo = 25;
+        System.out.println(rollNo); //reassining a value
+
+        int regno = 101;
+        System.out.println(regno);
+        regno = 105;
+        System.out.println(regno);
+
+        int team = 1;
+        System.out.println(team);
+        team = 2;
+        System.out.println(team);
+
+        int speed = 500;
+        System.out.println(speed);
+        speed = 100;
+        System.out.println(speed);
+
+        int weight = 40;
+        System.out.println(weight);
+        weight = 45;
+        System.out.println(weight);
+
+        int time = 10;
+        System.out.println(time);
+        time = 5;
+        System.out.println(time);
+
+        int marks = 88;
+        System.out.println(marks);
+        marks = 98;
+        System.out.println(marks);
+
+        int age = 21;
+        System.out.println(age);
+        age = 25;
+        System.out.println(age);
+
+        int population = 45000;
+        System.out.println(population);
+        population = 50000;
+        System.out.println(population);
+
+        int cell_price = 1300;
+        System.out.println(cell_price);
+        cell_price = 1500;
+        System.out.println(cell_price);
+
+        int adharcard_no = 3670; 
+        System.out.println(adharcard_no);
+        adharcard_no = 3670; 
+        System.out.println(adharcard_no);
+
+        int pincode_no = 459222;
+        System.out.println(pincode_no);
+        pincode_no = 459202;
+        System.out.println(pincode_no);
+
+        int phone_no = 7647;
+        System.out.println(phone_no);
+        phone_no = 7647;
+        System.out.println(phone_no);
+
+        int voltage = 220;
+        System.out.println(voltage);
+        voltage = 240;
+        System.out.println(voltage);
+
+        int price = 1000;
+        System.out.println(price);
+
+    }
+}
